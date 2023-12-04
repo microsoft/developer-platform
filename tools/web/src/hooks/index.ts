899809155037e44796d830c545f1182a958e6d13
Colby Williams <ColbyLWilliams@gmail.com>
2023-12-04 14:39:36 -0600

    delete web

--- tools/web/src/hooks/index.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-export * from './useEntities';
-export * from './useEntity';
-export * from './useGraphUser';
-export * from './useInterval';
-export * from './useProviderAuth';
-export * from './useTemplateCreate';
